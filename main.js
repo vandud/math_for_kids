aa99dfbf0d790f0ba833e1a3000f5e91e5633910
Ivan Dudin <van.dud.niga@yandex.ru>
2018-02-22 14:54:08 +0300

    Добавил счетчик неправильных ответов

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-var task = document.getElementById('task');
-var answer = document.getElementById('answer');
-var button = document.getElementById('button');
-var status = parseInt(0, 10);
-
+var right_answer = parseInt(0, 10);
+var not_right_answer = parseInt(0, 10);
 var num_one = 0;
 var num_two = 0;
 var operation = '';
+
+function start() {
+
+}
 
 function verify_answer() {
   var result = 0;
@@ -27,13 +28,14 @@ function verify_answer() {
       break;
   }
 
-  if (parseInt(answer.value, 10) == result) {
-    status++;
-    document.getElementById('status').innerHTML = status;
+  if (parseInt(document.getElementById('answer').value, 10) == result) {
+    right_answer++;
+    document.getElementById('right_answer').innerHTML = 'Правильных ответов: ' + right_answer;
     document.getElementById('answer').style.boxShadow = 'none';
-    console.log(status);
     gen_expression();
   } else {
+    not_right_answer++;
+    document.getElementById('not_right_answer').innerHTML = 'Не правильных ответов: ' + not_right_answer;
     document.getElementById('answer').style.boxShadow = '0 0 10px #f00 inset';
   }
 }
@@ -42,14 +44,14 @@ function gen_expression() {
   num_one = parseInt(Math.random() * 100 + 1);
   num_two = parseInt(Math.random() * 100 + 1);
   operation = ['+', '-', '*', '*'][parseInt(Math.random() * 4)];
-  task.innerHTML = num_one + ' ' + operation + ' ' + num_two + ' ' + '=';
+  document.getElementById('task').innerHTML = num_one + ' ' + operation + ' ' + num_two + ' ' + '=';
 }
 
 (function() {
   document.querySelector('input').addEventListener('keydown', function(e) {
  if (e.keyCode === 13) {
  verify_answer();
- answer.value = '';
+ document.getElementById('answer').value = '';
  }
   });
 })();
